d535c2d7056a9bfb7212f88213e44b00b6d1b775
Yesi Meza <yesi.meza10@gmail.com>
2021-03-23 10:47:48 -0600

    edited psacing

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  subject { Cart.new({}) }
+  subject { Cart.new({}) }       
 
   describe "instance methods" do
     it "#add_item" do
